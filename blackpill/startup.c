69a78f07ff6c6d16082e08296ef9b5162bf43208
PVVovan <pvvovan@gmail.com>
2022-05-26 09:36:51 +0200

    Init SRAM memory at startup

--- blackpill/startup.c
+++ blackpill/startup.c
@@ -3,14 +3,39 @@
 #include "main.h"
 
 
-void startup(void)
-{
+#define __BKPT(value)			__asm volatile ("bkpt "#value)
 
+extern volatile uint32_t _start_data;
+extern volatile uint32_t _end_data;
+extern volatile uint32_t _start_bss;
+extern volatile uint32_t _end_bss;
+extern const uint32_t _sidata;
+
+static void init_memory(void)
+{
+	volatile uint32_t *ram_ptr = &_start_data;
+	const uint32_t *flash_ptr = &_sidata;
+
+	while (ram_ptr <= &_end_data) {
+		*ram_ptr = *flash_ptr;
+		ram_ptr++;
+		flash_ptr++;
+	}
+
+	ram_ptr = &_start_bss;
+	while (ram_ptr <= &_end_bss) {
+		*ram_ptr = 0u;
+		ram_ptr++;
+	}
 }
 
 void reset_handler(void)
 {
-	startup();
+	__BKPT(0); // Set software breakpoint twice ;)
+	asm volatile ("bkpt 0");
+
+	init_memory();
+
 	main();
 	for ( ; ; ) {
 		;
@@ -24,13 +49,18 @@ void default_handler(void)
 	}
 }
 
-uint32_t isr_vector_table[121] __attribute__ ((section(".isr_vector"))) =
+const uint32_t isr_vector_table[116] __attribute__ ((section(".isr_vector"))) =
 {
 	(uint32_t)&reset_handler,
 
-	(uint32_t)&default_handler,
-	(uint32_t)&default_handler,
-	(uint32_t)&default_handler,
-	(uint32_t)&default_handler,
-	(uint32_t)&default_handler
+	(uint32_t)&default_handler,	// NMI		0x0000 0008
+	(uint32_t)&default_handler,	// HardFault	0x0000 000C
+	(uint32_t)&default_handler,	// MemManage	0x0000 0010
+	(uint32_t)&default_handler,	// BusFault	0x0000 0014
+	(uint32_t)&default_handler,	// UsageFault	0x0000 0018
+	0,				// Reserved	0x0000 001C - 0x0000 002B
+	0,
+	0,
+	0,
+	(uint32_t)&default_handler	// SVCall	0x0000 002C
 };
