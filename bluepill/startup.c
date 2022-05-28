10e36b03691cef33bd7d4e9447a24126420ae8ba
PVVovan <pvvovan@gmail.com>
2022-05-28 20:22:23 +0200

    Enable High Speed External clock

--- bluepill/startup.c
+++ bluepill/startup.c
@@ -30,6 +30,30 @@ static void init_memory(void)
 	}
 }
 
+static void init_system_clock(void)
+{
+	RCC->CR |= RCC_CR_HSEON; // Enable High Speed External clock
+	while ((RCC->CR & RCC_CR_HSERDY) == 0u) { } // wait until HSE oscillator is stable
+
+	FLASH->ACR |= FLASH_ACR_PRFTBE; // Enable the FLASH prefetch buffer
+	FLASH->ACR &= ~FLASH_ACR_LATENCY;
+	FLASH->ACR |= FLASH_ACR_LATENCY_2; // Set 2 wait states if 48MHz < SYSCLK <= 72MHz
+
+	RCC->CFGR |= RCC_CFGR_HPRE_DIV1; // Bits 7:4 HPRE: AHB prescaler
+	RCC->CFGR |= RCC_CFGR_PPRE2_DIV1; // Bits 13:11 PPRE2: APB high-speed prescaler (APB2)
+	RCC->CFGR |= RCC_CFGR_PPRE1_DIV2; // Bits 10:8 PPRE1: APB low-speed prescaler (APB1)
+	RCC->CFGR &= ~RCC_CFGR_PLLXTPRE; // Bit 17 PLLXTPRE: HSE divider for PLL entry
+	RCC->CFGR |= RCC_CFGR_PLLSRC; // Bit 16 PLLSRC: PLL entry clock source
+	RCC->CFGR |= RCC_CFGR_PLLMULL9; // Bits 21:18 PLLMUL: PLL multiplication factor
+
+	RCC->CR |= RCC_CR_PLLON; // Bit 24 PLLON: PLL enable
+	while ((RCC->CR & RCC_CR_PLLRDY) == 0u) { } // Bit 25 PLLRDY: PLL clock ready flag
+
+	RCC->CFGR &= ~RCC_CFGR_SW; // Bits 1:0 SW: System clock switch
+	RCC->CFGR |= RCC_CFGR_SW_PLL; // 10: PLL selected as system clock
+	while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL) { } // Bits 3:2 SWS: System clock switch status
+}
+
 void reset_handler(void)
 {
 	__BKPT(0); // Set software breakpoint twice ;)
@@ -37,7 +61,9 @@ void reset_handler(void)
 	asm volatile ("bkpt 0");
 
 	init_memory();
-	SysTick_Config(8000000);
+
+	init_system_clock();
+	SysTick_Config(72000000 / 10);
 
 	main();
 	for ( ; ; ) {
